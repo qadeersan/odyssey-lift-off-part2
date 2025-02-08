7dc46106eb4f202368af1fb000cd12b3619d54e9
qadeerassan <45184523+qadeerassan@users.noreply.github.com>
2025-02-08 16:29:02 -0500

    program resolvers

---
+++ server/src/resolvers.ts
@@ -0,0 +1,15 @@
+export const resolvers = {
+    Query: {
+        // returns an array of Tracks that will be used to populate
+        // the homepage grid of our web client
+        tracksForHome: (_, __, {dataSources}) => {
+            return dataSources.TrackAPI.getTracksForHome();
+        },
+        Track: {
+            author: ({authorId}, _, {dataSources}) => {
+                return dataSources.trackAPI.getAuthor(authorId);
+            }
+
+        }
+      }
+};
